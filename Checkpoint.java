c5107682e7d4e44f9bfec30ef32dbe9a11adb5b8
TylerLuitjens <tyluitjens@gmail.com>
2018-12-19 15:16:38 -0600

    Finishes Checkpoint and starts Flowerly

--- Checkpoint.java
+++ Checkpoint.java
@@ -2,22 +2,85 @@ import java.util.ArrayList;
 
 public class Checkpoint{
 	
-	private String text;
+	private String text, name;
 	private Checkpoint parent;
-	private ArrayList<Checkpoint> children;
-
+	private ArrayList<Checkpoint> children, parents;
+	private int x,y;
 	public Checkpoint(){
 		text = null;
-		parent = null;
+		name = null;
+		parents = new ArrayList<Checkpoint>();
 		children = new ArrayList<Checkpoint>();
+		x = -1;
+		y = -1;
 	}
 	
-	public void addText(String newText){
+	public void setX(int x){
+		this.x = x;
+	}
 
+	public int getX(){
+		return x;
+	}
 
+	public void setY(int y){
+		this.y = y;
+	}
+
+	public int getY(){
+		return y;
+	}
+
+	public void setLoc(int x, int y){
+		this.x = x;
+		this.y = y;
+	}
+
+	public void setText(String newText){
+		text = newText;
 	}
 	
+	public String getText(){
+		return text;
+	}
+
 	public void addChild(Checkpoint child){
 		children.add(child);
 	}
+	
+	public ArrayList<Checkpoint> getChildren(){
+		return children;
+	}
+
+	public void addParent(Checkpoint parent){
+		parents.add(parent);
+	}
+
+	public ArrayList<Checkpoint> getParents(){
+		return parents;
+	}
+	
+	public void setName(String name){
+		this.name = name;
+	}
+
+	public String getName(){
+		return name;
+	}
+
+	public String toString(){
+		String chkpt = "";
+		if(name != null)
+			chkpt = "Name: " + name + "\n";
+		else
+			chkpt = "--No name set-- \n";
+		if(text != null)
+			chkpt = chkpt.concat("  Contents: " + text + "\n");
+		else
+			chkpt = chkpt.concat("  --No text added--\n");
+		chkpt = chkpt.concat("  Number of parents: " + parents.size() + "\n");
+		chkpt = chkpt.concat("  Number of children: " + children.size() + "\n");
+		return chkpt;
+	}
+
 }
--- Flowerly.java
+++ Flowerly.java
@@ -1,4 +1,11 @@
-public class template{
+import javax.swing.JLabel;
+import java.awt.FlowLayout;
+import javax.swing.JFrame;
+public class Flowerly extends JFrame{
 	public static void main(String [] args){
+		Checkpoint chkpt = new Checkpoint();
+		chkpt.setText("This is a test entry for the checkpoint system. Hopefully these contents will someday hold part of a storyt that will be told interactively through the command line. The next step is to make the \n	GUI that will show the full tree of children and parents, as well as make a new child from a given parent.I also need to figure out how to print it to the terminal nicely without having words get cut in half, although that\n	 might get fixed with the gui itself being able to wrap the text nicely");
+		chkpt.setName("Test checkpoint");
+		System.out.println(chkpt.toString());
 	}
 }
