36c1a08b1927372a4aa2baa0d14b9d8a250f0076
TylerLuitjens <tyluitjens@gmail.com>
2019-04-18 14:14:16 -0500

    Adds framework for Shell Script

---
+++ shscript/basicscript.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+touch tempfile.txt
+chmod 777 tempfile.txt
+#export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:.:/appropriate/directory
