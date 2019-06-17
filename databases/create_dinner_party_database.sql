1b61594d848057c159618cfb63f27482dd86af0a
TylerLuitjens <tyluitjens@gmail.com>
2019-06-17 12:48:31 -0500

    Adds sql files for dinner party site

---
+++ databases/create_dinner_party_database.sql
@@ -0,0 +1,87 @@
+/*
+DROP DATABASE IF EXISTS DinnerParty;
+CREATE DATABASE DinnerParty; 
+/* */
+USE DinnerParty;
+DROP TABLE IF EXISTS UserGameFavorites;
+DROP TABLE IF EXISTS UserGameRating;
+DROP TABLE IF EXISTS UserGames;
+DROP TABLE IF EXISTS GamesGenres;
+DROP TABLE IF EXISTS Games;
+DROP TABLE IF EXISTS Genres;
+DROP TABLE IF EXISTS UserInformation;
+
+
+CREATE TABLE Genres(
+	genre_id INT AUTO_INCREMENT,
+    genre_name NVARCHAR(30) NOT NULL,
+    
+    PRIMARY KEY (genre_id)
+);
+
+CREATE TABLE Games(
+
+	game_id INT NOT NULL AUTO_INCREMENT,
+    game_name NVARCHAR(45) NOT NULL,
+    min_players INT,
+    max_players INT,
+    min_age INT,
+    
+	game_duration NVARCHAR(15),
+    game_description NVARCHAR(250),
+    game_price DECIMAL (4,2),
+    game_publisher NVARCHAR(25),
+    game_release_date DATE,
+    
+    game_img_url varchar(30), -- may want to make this into a linking table as well if I end up using multiple images per game
+    num_pieces INT,
+    game_instructions_url NVARCHAR(30),
+    
+    PRIMARY KEY (game_id)
+);
+
+CREATE TABLE UserInformation(
+	user_id INT AUTO_INCREMENT,
+    username NVARCHAR(30),
+    password NVARCHAR(30),
+    user_creation_date DATE,
+    
+    PRIMARY KEY (user_id)
+);
+
+CREATE TABLE GamesGenres(
+	game_id INT NOT NULL,
+    genre_id INT NOT NULL,
+    
+    PRIMARY KEY (game_id, genre_id),
+    FOREIGN KEY(game_id) REFERENCES Games(game_id),
+    FOREIGN KEY (genre_id) REFERENCES Genres(genre_id)
+);
+
+CREATE TABLE UserGames(
+	user_id INT,
+    game_id INT,
+    
+    PRIMARY KEY (user_id, game_id),
+    FOREIGN KEY (user_id) REFERENCES UserInformation (user_id),
+    FOREIGN KEY (game_id) REFERENCES Games (game_id)
+);
+
+CREATE TABLE UserGamesFavorites(
+	user_id INT,
+    game_id INT,
+    
+    PRIMARY KEY (user_id, game_id),
+    FOREIGN KEY (user_id) REFERENCES UserInformation (user_id),
+    FOREIGN KEY (game_id) REFERENCES Games (game_id)
+);
+
+CREATE TABLE UserGameRating(
+	user_id INT,
+    game_id INT,
+    rating INT,
+    
+    PRIMARY KEY (user_id, game_id),
+	FOREIGN KEY (user_id) REFERENCES UserInformation (user_id),
+    FOREIGN KEY (game_id) REFERENCES Games (game_id)
+);
---
+++ databases/fill_dinner_party_database.sql
@@ -0,0 +1,41 @@
+USE BoardGames;
+
+INSERT INTO Genres
+  (genre_name)
+  VALUES ('Card Game');
+INSERT INTO Genres
+  (genre_name)
+  VALUES ('Board Game');
+INSERT INTO Genres
+  (genre_name)
+  VALUES ('Puzzle');
+INSERT INTO Genres
+  (genre_name)
+  VALUES ('Strategy');
+INSERT INTO Genres
+  (genre_name)
+  VALUES ('Family');
+INSERT INTO Genres
+  (genre_name)
+  VALUES ('Co-op');
+INSERT INTO Genres
+  (genre_name)
+  VALUES ('Deck Building');
+INSERT INTO Genres
+  (genre_name)
+  VALUES ('Acting');
+INSERT INTO Genres
+  (genre_name)
+  VALUES ('Art');
+INSERT INTO Genres
+  (genre_name)
+  VALUES ('Memory');
+INSERT INTO Genres
+  (genre_name)
+  VALUES ('World Building');
+INSERT INTO Genres
+  (genre_name)
+  VALUES ('Worker Placement');
+INSERT INTO Genres
+  (genre_name)
+  VALUES ('Teams');
